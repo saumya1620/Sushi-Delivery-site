64da77279456740a448237892dded47a544f2e13
saumya1620 <122440288+saumya1620@users.noreply.github.com>
2024-05-22 09:16:31 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,35 @@
+import Header from './components/header/Header';
+import Slide1 from './components/component1/Slide1';
+import Slide2 from './components/component2/Slide2';
+import Slide3 from './components/component3/Slide3';
+import Slide4 from './components/component4/Slide4';
+import Slide5 from './components/component5/Slide5';
+import Slide6 from './components/component6/Slide6';
+import Slide7 from './components/component7/Slide7';
+import Footer from './components/component8/Footer';
+import './App.css'
+
+
+
+function App()
+{
+  return(
+    <div>
+      <Header/>
+      <Slide1/>
+      <Slide2/>
+      <Slide3/>
+      <Slide4/>
+      <Slide5/>
+      <Slide6/>
+      <Slide7/>
+      <Footer/>
+    
+ 
+      
+    
+    </div>
+    
+  );
+}
+export default App;
